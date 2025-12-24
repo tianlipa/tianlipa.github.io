95a63e8895c45408207974344f2b703fb9cfa050
Ye Sang <tianlipa@qq.com>
2025-12-24 15:56:53 +0800

    Site updated: 2025-12-24 15:56:49

--- js/subtitle.js
+++ js/subtitle.js
@@ -220,7 +220,8 @@ document.addEventListener("DOMContentLoaded", function () {
         "你变得懦弱了, 老东西",
         "WAS HERE",
         "只有风暴才能击倒大树",
-        "行不得也, 哥哥"
+        "行不得也, 哥哥",
+        "井, 井, 井"
     ];
     var randomIndex = Math.floor(Math.random() * paragraphs.length);
     var selectedParagraph = paragraphs[randomIndex];
